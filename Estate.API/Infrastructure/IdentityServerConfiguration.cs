93f0855f7604d8027c1e6611543dffc91d111731
Havrylei <havrylei.petro@gmail.com>
2019-02-01 09:50:22 +0200

    init

---
+++ Estate.API/Infrastructure/IdentityServerConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace Estate.API.Infrastructure
+{
+    internal static class IdentityServerConfiguration
+    {
+        internal const string SCOPE_NAME = "Estate.API";
+        internal const string CLIENT_NAME = "Estate.Client";
+
+        public static List<ApiResource> GetApis()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource(SCOPE_NAME)
+            };
+        }
+
+        public static List<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = CLIENT_NAME,
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("SuperSecret".Sha256())
+                    },
+                    AllowedScopes = { SCOPE_NAME }
+                }
+            };
+        }
+
+
+    }
+}
---
+++ Estate.API/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Estate.API.Infrastructure;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Estate.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddIdentityServer()
+                .AddInMemoryApiResources(IdentityServerConfiguration.GetApis())
+                .AddInMemoryClients(IdentityServerConfiguration.GetClients())
+                .AddDeveloperSigningCredential();
+
+            //services.AddAuthorization();
+
+            //services.AddAuthentication()
+            //    .AddJwtBearer("Bearer", option => {
+            //        option.Authority = "http://localhost:5000";
+            //        option.RequireHttpsMetadata = false;
+            //        option.Audience = IdentityServerConfiguration.SCOPE_NAME;
+            //    });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+            //app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
